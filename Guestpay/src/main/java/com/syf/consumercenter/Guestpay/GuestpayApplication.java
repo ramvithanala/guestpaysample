12443611af46a41e647f573cf44ea781ee59797d
Ram Vithanala <Ram Vithanala@DESKTOP-RFNQ0M7>
2017-10-28 21:02:55 +0530

    test

---
+++ Guestpay/src/main/java/com/syf/consumercenter/Guestpay/GuestpayApplication.java
@@ -0,0 +1,12 @@
+package com.syf.consumercenter.Guestpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestpayApplication.class, args);
+	}
+}
---
+++ Guestpay/src/test/java/com/syf/consumercenter/Guestpay/GuestpayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syf.consumercenter.Guestpay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuestpayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
